e5f4f060a29c85297eb536cad20f8613759b2ffe
Akane <96370213+akane6704@users.noreply.github.com>
2022-07-11 16:00:22 +0200

    Perfectioned the code a bit

--- script/install.sh
+++ script/install.sh
@@ -4,30 +4,28 @@ THEME_DIR="/usr/share/grub/themes"
 THEME_NAME="AvdanOS-Grub-Theme"
 
 
-
-#colors
-
-CDEF=" \033[0m"     # default color
-CCIN=" \033[0;36m"  # info color
-CGSC=" \033[0;32m"  # success color
-CRER=" \033[0;31m"  # error color
-CWAR=" \033[0;33m"  # warning color
-b_CDEF=" \033[1;37m"# bold default color
-b_CCIN=" \033[1;36m"# bold info color
-b_CGSC=" \033[1;32m"# bold success color
-b_CRER=" \033[1;31m"# bold error color
+# Colors
+CDEF=" \033[0m"      # Default color
+CCIN=" \033[0;36m"   # Info color
+CGSC=" \033[0;32m"   # Success color
+CRER=" \033[0;31m"   # Error color
+CWAR=" \033[0;33m"   # Warning color
+b_CDEF=" \033[1;37m" # Bold default color
+b_CCIN=" \033[1;36m" # Bold info color
+b_CGSC=" \033[1;32m" # Bold success color
+b_CRER=" \033[1;31m" # Bold error color
 b_CWAR=" \033[1;33m"
 
 prompt () {
     case ${1} in
         "-s"|"--success")
-        echo -e "${b_CGSC}${@/-s/}${CDEF}";;          # print success message
+        echo -e "${b_CGSC}${@/-s/}${CDEF}";; # Print success message
         "-e"|"--error")
-        echo -e "${b_CRER}${@/-e/}${CDEF}";;          # print error message
+        echo -e "${b_CRER}${@/-e/}${CDEF}";; # Print error message
         "-w"|"--warning")
-        echo -e "${b_CWAR}${@/-w/}${CDEF}";;          # print warning message
+        echo -e "${b_CWAR}${@/-w/}${CDEF}";; # Print warning message
         "-i"|"--info")
-        echo -e "${b_CCIN}${@/-i/}${CDEF}";;          # print info message
+        echo -e "${b_CCIN}${@/-i/}${CDEF}";; # Print info message
         *)
             echo -e "$@"
         ;;
@@ -35,33 +33,32 @@ prompt () {
 }
 
 
-# checking command availability
+# Checking command availability
 function has_command() {
     command -v $1 > /dev/null
 }
 
-#checking for root access
+# Checking for root access
 prompt -w "\nChecking for root access...\n"
+
 if [ "$UID" -eq "$ROOT_UID" ]; then
+
     # Create themes directory if not exists
-    prompt -i "\nChecking for the existence of themes directory...\n"
     [[ -d ${THEME_DIR}/${THEME_NAME} ]] && rm -rf ${THEME_DIR}/${THEME_NAME}
     mkdir -p "${THEME_DIR}/${THEME_NAME}"
+    prompt -i "\nChecking for the existence of themes directory...\n"
     
-    
-    #copy theme
-    prompt -i "\nInstalling the theme...\n"
+    # Copy theme   
     cp -a ../src/* ${THEME_DIR}/${THEME_NAME}
+    prompt -i "\nInstalling the theme...\n"
     
-    
-    #set theme
+    # Set theme
     prompt -i "\nSetting ${THEME_NAME} as default...\n"
     
     # Backup grub config
     cp -an /etc/default/grub /etc/default/grub.bak
     
     grep "GRUB_THEME=" /etc/default/grub 2>&1 >/dev/null && sed -i '/GRUB_THEME=/d' /etc/default/grub
-    
     echo "GRUB_THEME=\"${THEME_DIR}/${THEME_NAME}/theme.txt\"" >> /etc/default/grub
     
     
@@ -73,12 +70,8 @@ if [ "$UID" -eq "$ROOT_UID" ]; then
     
     
     # Success message
-    prompt -s "\n\t          ****************************\n\t          * Grub theme successfully installed!  *\n\t          ****************************\n"
-    
+    prompt -s "\n\t          ****************************\n\t          * Grub theme successfully installed!  *\n\t          ****************************\n" 
 else
-    
     # Error message
-    prompt -e "\n [ Error! ] -> For installing the grub theme you have to be root! \n \n "
-    
+    prompt -e "\n [ Error! ] -> For installing the grub theme you have to be root! \n \n " 
 fi
-
